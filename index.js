ad350a90b3cdcd84a97f6eaf2d077bfecf9a0389
Jatin Kumar <14jatinkumar09@gmail.com>
2025-11-29 17:54:27 +0530

    ENV change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 
  app.use(cors({
-  origin: 'http://localhost:5173' , //'https://todo-frontend-react-production.vercel.app'
+  origin: process.env.URI , //'https://todo-frontend-react-production.vercel.app'
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 })); 
